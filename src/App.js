678d5625afa0482171e689dded19c5ef7786695f
Marcos Paulo Guida <mp1911@hotmail.com.br>
2024-09-24 15:43:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
+
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = "6ac174ee019af23cadcac8fe0442832a";
+
+const endpoints = {
+  originals: "/discover/tv",
+  trending: "/trending/all/week",
+  now_playing: "/movie/now_playing",
+  popular: "/movie/popular",
+  top_rated: "/movie/top_rated",
+  upcoming: "/movie/upcoming",
+};
+
+function Image(image) {
+  console.log("TESTE: ", image.image.poster_path);
+  return (
+    <img src={`https://image.tmdb.org/t/p/w300${image.image.poster_path}`} alt="Movie Poster" />
+  )
+
+}
+
 function App() {
+
+  const [originals, setOriginals] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);  
+
+  useEffect(() => {
+    // Load Originals
+    axios
+      .get(`${URL}${endpoints.originals}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => {
+        console.log("resultado: ", res.data.results);
+        setOriginals(res.data.results)});
+  
+    // Get other categories with the same pattern here
+  
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {originals.map((image) => <Image image={image}/>)}
     </div>
   );
 }
